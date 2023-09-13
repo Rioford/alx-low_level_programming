c39e6c8d785408d201fe7d31182df613ae3068c1
Rioford <sixcap3@gmail.com>
2023-09-12 18:42:58 -0700

    2-alphabet_x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,19 +5,19 @@
  * Return: void
  */
 void print_alphabet_x10(void)
-
 {
-	char c;
-        int i = 0;
 
-        while (i <= 9)
-        
-        for (c = 'a'; c <= 'z'; c++)
-        {
-        _putchar(c);
-
-        _putchar('\n');
-        i++;
-        }
+char ch;
+int i;
+i = 0;
+while (i < 10)
+{
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+_putchar(ch);
+}
+_putchar('\n');
+i++;
+}
 
 }
