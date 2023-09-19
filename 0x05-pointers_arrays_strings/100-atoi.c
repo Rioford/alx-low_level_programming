105fae9be0fbecf8aa497f00171fd2b60e47f330
Rioford <sixcap3@gmail.com>
2023-09-18 20:08:32 -0700

    100-atoi

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,12 +1,13 @@
 #include "main.h"
 
 /**
- * _atoi - convert a string to an integer
+ * _atoi - Convert a string to an integer
  * @s: The pointer to convert
- *
- * Returrn: An integer
+ * Return: A integer
  */
+
 int _atoi(char *s)
+
 {
 
 	int c = 0;
